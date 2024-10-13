ab3652a7ece5105b681618d7ccd2a45666320037
Mohammad Iqbal <ki925053@gmail.com>
2024-10-14 02:23:19 +0530

    db connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './db/index.js';
+dotenv.config({
+    path:'./env'
+})
+const app = express();
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello Iqbal')
@@ -9,3 +14,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//database connection function
+
+connectDB();
